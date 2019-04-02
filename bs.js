616833c9c1180d1fa294a7e728647040f28f881b
Cristi C <cristian.cimpean@plentydevelopment.com>
2019-04-02 12:27:56 +0300

    config rename

--- bs.js
+++ bs.js
@@ -1,31 +1,31 @@
 var bs = require('browser-sync').create();
 
-    bs.init({
-        proxy: "https://codecamp.plentymarkets-cloud01.com/ItList",
-        files: [
-            'resources/js/**',
-            'resources/css/**',
-        ],
+bs.init({
+    proxy: "https://codecamp.plentymarkets-cloud01.com/ItList",
+    files: [
+        'resources/js/**',
+        'resources/css/**',
+    ],
 
-        serveStatic: [
-            {
+    serveStatic: [
+        {
             route: ['/resources/js'],
             dir: 'resources/js'
-            },
-            {
-                route: ['/resources/css'],
-                dir: 'resources/css'
-            },
-        ],
+        },
+        {
+            route: ['/resources/css'],
+            dir: 'resources/css'
+        },
+    ],
 
-        rewriteRules: [
-            {
+    rewriteRules: [
+        {
             match: new RegExp('http.*\\/itlist\\/js\\/(.*.js)'),
             replace: '/resources/js/$1'
-            },
-            {
-                match: new RegExp('http.*\\/itlist\\/css\\/(.*.css)'),
-                replace: '/resources/css/$1'
-            },
-        ]
-    });
+        },
+        {
+            match: new RegExp('http.*\\/itlist\\/css\\/(.*.css)'),
+            replace: '/resources/css/$1'
+        },
+    ]
+});
