91b1e55d3f0b7358ebed99560ee32c28c303fd14
Cristi C <cristian.cimpean@plentydevelopment.com>
2019-04-08 15:10:06 +0300

    dependency plugin

--- resources/js/item/prodImage.js
+++ resources/js/item/prodImage.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
     $(".prod_image").each(function() {
-        console.log(this);
-        console.log();
-
         if(!$(this).attr("src").length) {
-            console.log('OverWrite');
-            console.log(this.src);
             this.src = 'http://clipart-library.com/image_gallery/267356.png'
         }
     });
